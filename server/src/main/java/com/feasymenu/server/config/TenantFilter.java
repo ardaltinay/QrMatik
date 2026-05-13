36903d841e7b43e0921ec4e7fcbf35714ddcc80b
Arda Altınay <ardaltinay@Mac.hgw.local>
2026-05-13 17:08:28 +0300

    optimize imports

--- server/src/main/java/com/feasymenu/server/config/TenantFilter.java
+++ server/src/main/java/com/feasymenu/server/config/TenantFilter.java
@@ -13,7 +13,6 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-
 @Component
 public class TenantFilter extends OncePerRequestFilter {
     private final TenantRepository tenantRepository;
